f4aa0b40e01b6a4fa56b657ed6c2030e006b25fe
Hunter White hrwhite@crimson.ua.edu <hrwhite@crimson.ua.edu>
2024-04-11 17:43:56 -0500

    worked through a few issues with syntax and assigning values properly ~NOTE~ TOWN03_Opt has an issue with the traffic manager that crashes the script. Have not found why.

--- EgoGen_DataCollection.py
+++ EgoGen_DataCollection.py
@@ -3,8 +3,16 @@ import numpy as np
 import os
 import platform
 import random
-import pygame
+import glob
+import sys
 
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
 cc = carla.ColorConverter.CityScapesPalette
 
 
@@ -30,35 +38,42 @@ def invasion_callback(lane_invasion_data):
     return
 
 
-desired_town = 'Town03_Opt'
+desired_town = 'Town03'
 desired_fps = 30
 client = carla.Client('localhost', 2000)
-#client.load_world(desired_town)
-
+client.set_timeout(15)
+client.load_world_if_different(desired_town)
 # Create and store all high level objects
 world = client.get_world()
 level = world.get_map()
 weather = world.get_weather()
 blueprint_library = world.get_blueprint_library()
 settings = world.get_settings()
+
+my_tm = client.get_trafficmanager(8000)
 # Enabling synchronous mode required to get info from same timestep/frame when working with multiple sensors
 settings.synchronous_mode = True  # Enables synchronous mode
+my_tm.set_synchronous_mode(True)
 # FPS = 1/fixed_delta_seconds
+''' SO, IT FAILS HERE '''
+
 settings.fixed_delta_seconds = np.round(1 / desired_fps, 3)
-traffic_manager = client.get_trafficmanager()
 world.apply_settings(settings)
 
-if platform.system == 'Windows':
+spectator = world.get_spectator()
+
+print('\n', platform.system())
+if platform.system() == 'Windows':
     os_save_path = "C:/CarlaGitHub/RL_CARLA_ADAS/SavedData"
     sensor_os_save_path = "C:/CarlaGitHub/RL_CARLA_ADAS/SavedData/Sensors"
-elif platform.system == 'Ubuntu':
+elif platform.system() == 'Linux':
     os_save_path = "/data/HunterWhite/CarlaUE4/Recordings"
-    sensor_os_save_path = "/data/HunterWhite/CarlaUE4/Recordings/Sensors"
+    sensor_os_save_path = "~/data/HunterWhite/CarlaUE4/Recordings/Sensors"
+print(sensor_os_save_path)
 try:
     os.makedirs(sensor_os_save_path)
 except:
     pass
-
 
 
 # As an estimate, 1h recording with 50 traffic lights and 100 vehicles takes around 200MB in size.
@@ -68,34 +83,33 @@ ego_bp = blueprint_library.find('vehicle.mini.cooper_s_2021')
 ego_bp.set_attribute('role_name', 'ego')
 ego_color = random.choice(ego_bp.get_attribute('color').recommended_values)
 ego_bp.set_attribute('color', ego_color)
-ego_transform = random.shuffle(spawn_points)
+ego_transform = random.choice(spawn_points)
 ego_vehicle = world.try_spawn_actor(ego_bp, ego_transform)
 
 vehicles_bp = blueprint_library.filter('*vehicle*')
 
-camera_bp = blueprint_library.find('sensor.camera.rgb')
-camera_bp.set_attribute('image_size_x' '1280')
-camera_bp.set_attribute('image_size_y' '720')
-camera_bp.set_attribute('fov' '105')
+camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
+camera_bp.set_attribute('image_size_x', str(600))
+camera_bp.set_attribute('image_size_y', str(600))
 camera_location = carla.Location(2, 0, 1)
-camera_rotation = carla.Rotation(0, 180, 0)
+camera_rotation = carla.Rotation(0, 0, 0)
 camera_transform = carla.Transform(camera_location, camera_rotation)
 
 segment_camera_bp = blueprint_library.find('sensor.camera.semantic_segmentation')
-segment_camera_bp.set_attribute('image_size_x' '840')
-segment_camera_bp.set_attribute('image_size_y' '600')
-segment_camera_bp.set_attribute('fov' '90')
+segment_camera_bp.set_attribute('image_size_x','840')
+segment_camera_bp.set_attribute('image_size_y','600')
+segment_camera_bp.set_attribute('fov','90')
 segment_camera_location = carla.Location(1.8, 0, 1.2)
-segment_camera_rotation = carla.Rotation(0, 180, 0)
+segment_camera_rotation = carla.Rotation(0, 0, 0)
 segment_camera_transform = carla.Transform(segment_camera_location, segment_camera_rotation)
 
 collision_sensor_bp = blueprint_library.find('sensor.other.collision')
-collision_sensor_location = carla.location(0, 0, 0)
+collision_sensor_location = carla.Location(0, 0, 0)
 collision_sensor_rotation = carla.Rotation(0, 0, 0)
 collision_sensor_transformation = carla.Transform(collision_sensor_location, collision_sensor_rotation)
 
 lane_invasion_sensor_bp = blueprint_library.find('sensor.other.lane_invasion')
-invasion_sensor_location = carla.location(0, 0, 0)
+invasion_sensor_location = carla.Location(0, 0, 0)
 invasion_sensor_rotation = carla.Rotation(0, 0, 0)
 invasion_sensor_transformation = carla.Transform(invasion_sensor_location, invasion_sensor_rotation)
 
@@ -118,7 +132,9 @@ else:
                                                 attach_to=ego_vehicle, attachment_type=carla.AttachmentType.Rigid)
         ego_invasion_sensor.listen(lambda inv_info: invasion_callback(inv_info))
 
-        print('\n Error Loading Vehicle Sensors')
+        print('\n Loading Vehicle Sensors')
+    except:
+        print('\n Failed Loading Sensors!!')
     finally:
         print('\n Loading traffic!')
 
@@ -141,25 +157,33 @@ for i, spawn_point in enumerate(random.sample(spawn_points, max_vehicles)):
         vehicles.append(temp)
     for vehicle in vehicles:
         vehicle.set_autopilot(True)
+print('\n World Set to AutoPilot!')
+ego_vehicle.set_autopilot(True)
 
 try:
-    client.start_recorder(os_save_path, True)
+   # client.start_recorder(os_save_path, True)
     while True:
         world.tick()
         # Rest of Code to capture images
 
 except KeyboardInterrupt:
     print('\n Data Collection Terminated, destroying actors and reloading world')
-    for actor in carla.world.get_actors():
-        try:
-            actor.stop()
-            actor.destroy()
-        except:
-            actor.destoy()
+
+    active_sensors = world.get_actors().filter('sensor.*')
+    for sensor in active_sensors:
+        if sensor.is_listening:
+            sensor.stop()
+        sensor.destroy()
+
+    # Destroy vehicles
+    active_vehicles = world.get_actors().filter('vehicle.*')
+    for vehicle in active_vehicles:
+        vehicle.destroy()
+
 
 finally:
-    if carla.world.get_actors() is not None:
+    if world.get_actors().filter('sensor.*') or world.get_actors().filter('vehicle.*') is not None:
         print('\n The following actors were not destroyed. Please manually delete them.\n')
-        print(carla.world.get_actors())
+        print(world.get_actors())
 
     # DO the stuff to destroy the actors and free up computer resources.
