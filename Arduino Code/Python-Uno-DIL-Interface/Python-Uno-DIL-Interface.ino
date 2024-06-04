84abca3ff688ea5d2448f358040c456434d72dbd
hrwhite21 <80989120+hrwhite21@users.noreply.github.com>
2024-06-03 19:21:38 -0500

    Re-adds global variables
    I really shouldn't edit these things directly

--- Arduino Code/Python-Uno-DIL-Interface/Python-Uno-DIL-Interface.ino
+++ Arduino Code/Python-Uno-DIL-Interface/Python-Uno-DIL-Interface.ino
@@ -1,6 +1,32 @@
+// const int APPstepPin = 2;  // Step pin connected to digital pin 2
+// const int APPdirPin = 3;   // Direction pin connected to digital pin 3
+// const int BPPstepPin = 4;  // Step pin connected to digital pin 4
+// const int BPPdirPin = 5;   // Direction pin connected to digital pin 5
+// const int SWstepPin = 6;   // Step pin connected to digital pin 6
+// const int SWdirPin = 7;    // Direction pin connected to digital pin 7
+
+const int delayMicros = 700; // 5000 microseconds = 5 milliseconds
+const int APPMaxDisplacement = 690; 
+const int BPPMaxDisplacement = 660;
+const int SWMaxDisplacement = 400;
+
+int APPPos = 0;
+int BPPPos = 0;
+int SWPos = 0;
+
 const int bufferSize = 64; // Adjust buffer size as needed
 char inputBuffer[bufferSize];
 int bufferIndex = 0;
+
+void setup() {
+  // Set pins as outputs
+  DDRD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5) | (1 << PD6) | (1 << PD7);
+
+  // Set direction pins to HIGH
+  PORTD |= (1 << PD3) | (1 << PD5) | (1 << PD7);
+
+  Serial.begin(19200);
+}
 
 void loop() {
   while (Serial.available() > 0) {
@@ -8,12 +34,12 @@ void loop() {
     if (c == '\n') {
       inputBuffer[bufferIndex] = '\0'; // Null-terminate the string
       bufferIndex = 0;
-      
+
       int values[3];
       parseData(inputBuffer, values);
       constrainVals(values);
       StepHandler(values);
-      
+
       // Use a more efficient method to form the output string
       char output[64];
       sprintf(output, " A: %d B: %d SW: %d", APPPos, BPPPos, SWPos);
@@ -31,7 +57,7 @@ void parseData(char* data, int values[]) {
   char* ptr = data;
   while (*ptr != '\0' && valueidx < 3) {
     values[valueidx++] = strtol(ptr, &ptr, 10);
-    if (*ptr == ',') ptr++; // Skip the comma
+    if (*ptr == ',') ptr++; // Skip the commas
   }
 }
 
